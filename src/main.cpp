08684d5337b12457a12f18e47973a565169f3538
dwainwright999 <49505567+dwainwright999@users.noreply.github.com>
2020-04-13 21:03:14 -0400

    Added serial output of elapsed time

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ Bronco Racing 2020 Dyno zDAQ - See README for details
 
 // servo calibration: 70 = 0 degrees, 160 = 90 degrees
 
-
 #include <Hx711.h>
 #include <PID.h>
 #include <mbed.h>
@@ -34,17 +33,21 @@ float servoMax = 160;
 volatile int servoVal = servoMin;
 float RPMSet = 4500;
 
-volatile unsigned long tock = 1;
 
 CANMessage inMsg;
 CANMessage outMsg;
 
+Timer dataTimer;
 Timer canTimer;
+
 Servo servo(A2);
 
 void CANCallback();
 
-int main() {
+int main()
+{
+
+  dataTimer.start();
 
   controller.setInputLimits(0.0, 14000);
   controller.setOutputLimits(servoMax, servoMin);
@@ -58,7 +61,8 @@ int main() {
   outMsg.id = 0;
   outMsg.len = 8;
 
-  for (int i = 0; i < outMsg.len; i++) {
+  for (int i = 0; i < outMsg.len; i++)
+  {
     outMsg.data[i] = 0;
   }
 
@@ -75,55 +79,62 @@ int main() {
   can0.filter(0x0CFFF048, 0x0CFFF548, CANExtended, 0);
   can0.attach(CANCallback);
 
-  while (1) {
+  while (1)
+  {
     // Read scale and remove decimal point. Remove negative vals.
     scaleInt = (uint32_t)((abs(scale1.readTaredA())) * 1000);
     controller.setProcessValue((float)rpm);
 
-    if (rpm > (RPMSet - 1500)) {
+    if (rpm > (RPMSet - 1500))
+    {
       servoVal = servoMax - controller.compute();
       // servoVal = 40 ; //change the niumber here to change open angle (0 --> 140 = 0 --> 90)
-    } else {
+    }
+    else
+    {
       servoVal = servoMin;
     }
 
     servo.write(servoVal);
 
     // Send CAN alive frame
-    if (canTimer.read_ms() > 50) {
+    if (canTimer.read_ms() > 50)
+    {
       can0.write(outMsg);
       canTimer.reset();
     }
 
     // Send Data for plotting
-    usb.printf("%lu,", tock);
+    usb.printf("%f,", dataTimer.read());
     usb.printf("%d,", rpm);
     usb.printf("%ld\n", scaleInt);
-    tock++;
 
     // For terminal viewing
     // usb.printf("%f\t", (servoVal - servoMin)); // corrected valve position
     // usb.printf("%d\t", rpm);
     // usb.printf("%ld\n", scaleInt);
     // usb.printf("%d\n", waterTemp);
-
   }
 }
 
-
 // Fires on incoming CAN frame interrupt
-void CANCallback() {
-  if (can0.read(inMsg)) {
+void CANCallback()
+{
+  if (can0.read(inMsg))
+  {
     // Read in ECU frames
     // PE1
-    if (inMsg.id == 0x0CFFF048) {
+    if (inMsg.id == 0x0CFFF048)
+    {
       rpm = ((inMsg.data[1] << 8) + inMsg.data[0]);
       led = !led;
     }
     // PE6
-    else if (inMsg.id == 0x0CFFF548) {
+    else if (inMsg.id == 0x0CFFF548)
+    {
       uint16_t newTemp = ((inMsg.data[5] << 8) + inMsg.data[4]);
-      if (newTemp > 32767) {
+      if (newTemp > 32767)
+      {
         newTemp = newTemp - 65536;
       }
       // waterTemp = (uint16_t)((newTemp / 10) * 1.8) + 32;
